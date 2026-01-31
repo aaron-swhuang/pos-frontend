a8d515d0851d86ea8f957970fbe78bc869a5a5b4
ShaoWei Huang <aaron0327@gmail.com>
2026-02-01 02:10:41 +0800

    Refine vite.config.js to setup environment for ui test
    Set jsdom to simulate browser and add setup.js file.
    The setup.js file imports testing library jest-dom.
    
    Signed-off-by: ShaoWei Huang <aaron0327@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom', // 必須設定為 jsdom 以模擬瀏覽器
+    setupFiles: './src/tests/setup.js',
+  },
+});
